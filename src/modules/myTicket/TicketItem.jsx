3ecd86878de18fe0fe121b35cd582ab9e2bbdef5
Nguyen Van An <vangtienchi@gmail.com>
2024-07-24 17:59:12 +0700

    p

--- src/modules/myTicket/TicketItem.jsx
+++ src/modules/myTicket/TicketItem.jsx
@@ -1,32 +1,78 @@
-import { Button, Tag } from 'antd';
-import React from 'react';
+import {
+    convertDatetimeAndFormat,
+    formatDate,
+    formatTime,
+    subtractDateTimes,
+} from "@/utils/date";
+import { Button, notification, Tag } from "antd";
+import React, { useState } from "react";
 
-const TicketItem = () => {
-  return (
-    <div className="w-full h-[230px] p-5 bg-white rounded-lg">
-      <div className="flex flex-col w-full h-full border rounded-lg">
-        <div className="relative flex-1 p-5">
-          <div className="flex flex-col gap-2">
-            <span className="font-medium">T3, 09/07/2024</span>
-            <span className="font-medium text-[28px]">21:03</span>
-            <span>Hải Phòng travel</span>
-            <span>Hà Nội - Hải Phòng</span>
-          </div>
-          <div className="absolute top-2 right-2">
-            <Tag color="red">Đã hủy</Tag>
-          </div>
+const TicketItem = ({ currentTab, ticket }) => {
+    const [evaluation, setEvaluation] = useState(null);
+
+    const handleEvaluationClick = () => {
+        notification.info({
+            message: "Thông báo",
+            description: "Vui lòng tải ứng dụng để thực hiện đánh giá.",
+        });
+    };
+
+    return (
+        <div className='w-full h-[230px] p-5 bg-white rounded-lg'>
+            <div className='flex flex-col w-full h-full border rounded-lg'>
+                <div className='relative flex-1 p-5'>
+                    <div className='flex flex-col gap-2'>
+                        <span className='font-medium'>
+                            {formatDate(
+                                subtractDateTimes(ticket.trip.departure_time)
+                            )}
+                        </span>
+                        <span className='font-medium text-[28px]'>
+                            {formatTime(
+                                convertDatetimeAndFormat(
+                                    ticket.trip.departure_time
+                                )
+                            )}
+                        </span>
+                        <span>{ticket.trip.car.transport_company.name}</span>
+                        <div className='flex-row items-center '>
+                            <span>{ticket.trip.route_start}</span>
+                            <span> -</span>
+                            <span>{ticket.trip.route_end}</span>
+                        </div>
+                    </div>
+                    {currentTab === "cancel" && (
+                        <div className='absolute top-2 right-2'>
+                            <Tag color='red'>Đã hủy</Tag>
+                        </div>
+                    )}
+                </div>
+                {currentTab !== "cancel" && (
+                    <div>
+                        {currentTab === "past" ? (
+                            <div className='h-[50px] flex items-center font-medium gap-1 justify-end'>
+                                <button
+                                    onClick={handleEvaluationClick}
+                                    className='flex items-center justify-center w-[100%] h-full text-white bg-yellow-400 rounded-lg'
+                                >
+                                    Đánh giá
+                                </button>
+                            </div>
+                        ) : (
+                            <div className='h-[50px] flex items-center font-medium gap-1 justify-around'>
+                                <button className='flex items-center justify-center w-full h-full text-white bg-red-500 rounded-lg'>
+                                    Hủy
+                                </button>
+                                <button className='flex items-center justify-center w-full h-full text-white bg-yellow-400 rounded-lg'>
+                                    Thanh toán
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                )}
+            </div>
         </div>
-        <div className="h-[70px] flex items-center font-medium gap-1 justify-around">
-          <button className="flex items-center justify-center w-full h-full text-white bg-red-500 rounded-lg">
-            Hủy
-          </button>
-          <button className="flex items-center justify-center w-full h-full text-white bg-yellow-400 rounded-lg">
-            Thanh toán
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default TicketItem;
--- src/utils/date.js
+++ src/utils/date.js
@@ -1,128 +1,131 @@
-import moment from 'moment';
+import moment from "moment";
 
 export function getTimeFromDatetime(datetimeString) {
-  if (!datetimeString) return '';
-  const datetime = moment(datetimeString, 'YYYY-MM-DD HH:mm:ss');
-  if (!datetime.isValid()) {
-    throw new Error('Invalid datetime string');
-  }
-  const hours = datetime.hour().toString().padStart(2, '0');
-  const minutes = datetime.minute().toString().padStart(2, '0');
-  return `${hours}:${minutes}`;
+    if (!datetimeString) return "";
+    const datetime = moment(datetimeString, "YYYY-MM-DD HH:mm:ss");
+    if (!datetime.isValid()) {
+        throw new Error("Invalid datetime string");
+    }
+    const hours = datetime.hour().toString().padStart(2, "0");
+    const minutes = datetime.minute().toString().padStart(2, "0");
+    return `${hours}:${minutes}`;
 }
 
 export function subtractDateTimes(datetime1, datetime2) {
-  if (!datetime1 || !datetime2) return '';
-  const format = 'YYYY-MM-DD HH:mm:ss';
+    if (!datetime1 || !datetime2) return "";
+    const format = "YYYY-MM-DD HH:mm:ss";
 
-  // Chuyển đổi chuỗi thời gian thành đối tượng moment
-  const moment1 = moment(datetime1, format);
-  const moment2 = moment(datetime2, format);
+    // Chuyển đổi chuỗi thời gian thành đối tượng moment
+    const moment1 = moment(datetime1, format);
+    const moment2 = moment(datetime2, format);
 
-  // Kiểm tra tính hợp lệ của thời gian
-  if (!moment1.isValid() || !moment2.isValid()) {
-    throw new Error('Invalid datetime string');
-  }
+    // Kiểm tra tính hợp lệ của thời gian
+    if (!moment1.isValid() || !moment2.isValid()) {
+        throw new Error("Invalid datetime string");
+    }
 
-  // Tính toán sự chênh lệch
-  const duration = moment.duration(moment1.diff(moment2));
-  const hours = Math.abs(Math.floor(duration.asHours()));
-  const minutes = Math.abs(duration.minutes());
+    // Tính toán sự chênh lệch
+    const duration = moment.duration(moment1.diff(moment2));
+    const hours = Math.abs(Math.floor(duration.asHours()));
+    const minutes = Math.abs(duration.minutes());
 
-  // Tạo chuỗi kết quả
-  let result = '';
-  if (hours > 0) {
-    result += `${hours}h`;
-  }
-  if (minutes > 0) {
-    result += `${minutes}m`;
-  }
+    // Tạo chuỗi kết quả
+    let result = "";
+    if (hours > 0) {
+        result += `${hours}h`;
+    }
+    if (minutes > 0) {
+        result += `${minutes}m`;
+    }
 
-  // Nếu không có giờ và phút, trả về "0m"
-  if (result === '') {
-    result = '0m';
-  }
+    // Nếu không có giờ và phút, trả về "0m"
+    if (result === "") {
+        result = "0m";
+    }
 
-  return result;
+    return result;
 }
 
 export const formatDate = (dateString) => {
-  const date = dateString ? new Date(dateString) : new Date();
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = date.getFullYear();
-  const dayOfWeek = ['CN', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7'][date.getDay()];
-  return `${dayOfWeek}, ${day}/${month}/${year}`;
+    const date = dateString ? new Date(dateString) : new Date();
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    const dayOfWeek = ["CN", "T2", "T3", "T4", "T5", "T6", "T7"][date.getDay()];
+    return `${dayOfWeek}, ${day}/${month}/${year}`;
 };
 
 export const formatTime = (dateTimeString) => {
-  if (!dateTimeString) {
-    return '...';
-  }
+    if (!dateTimeString) {
+        return "...";
+    }
 
-  const dateTimeParts = dateTimeString?.split(' ');
-  if (dateTimeParts.length !== 2) {
-    console.error('Invalid datetime format');
-    return 'Invalid time';
-  }
+    const dateTimeParts = dateTimeString.split(" ");
+    if (dateTimeParts.length !== 2) {
+        console.error("Invalid datetime format");
+        return "Invalid time";
+    }
 
-  const timePart = dateTimeParts[1];
-  const time = new Date(`1970-01-01T${timePart}Z`); // Dùng ngày giả định để tạo đối tượng thời gian
+    const timePart = dateTimeParts[1];
+    const time = new Date(`1970-01-01T${timePart}Z`); // Dùng ngày giả định để tạo đối tượng thời gian
 
-  if (isNaN(time)) {
-    console.error('Invalid time format');
-    return 'Invalid time';
-  }
+    if (isNaN(time)) {
+        console.error("Invalid time format");
+        return "Invalid time";
+    }
 
-  const hours = String(time.getUTCHours()).padStart(2, '0');
-  const minutes = String(time.getUTCMinutes()).padStart(2, '0');
+    const hours = String(time.getUTCHours()).padStart(2, "0");
+    const minutes = String(time.getUTCMinutes()).padStart(2, "0");
 
-  return `${hours}:${minutes}`;
+    return `${hours}:${minutes}`;
 };
 
 export const convertDatetimeToServer = (inputStr) => {
-  if (!inputStr) return;
-  // Sử dụng Moment.js để phân tích chuỗi đầu vào
-  var momentObj = moment(inputStr, 'DD/MM/YYYY HH:mm:ss');
+    if (!inputStr) return;
+    // Sử dụng Moment.js để phân tích chuỗi đầu vào
+    var momentObj = moment(inputStr, "DD/MM/YYYY HH:mm:ss");
 
-  // Trừ 7 giờ từ thời gian
-  momentObj.subtract(7, 'hours');
+    // Trừ 7 giờ từ thời gian
+    momentObj.subtract(7, "hours");
 
-  // Chuyển đổi đối tượng Moment trở lại chuỗi với định dạng mong muốn
-  var outputStr = momentObj.format('YYYY-MM-DD HH:mm:ss');
+    // Chuyển đổi đối tượng Moment trở lại chuỗi với định dạng mong muốn
+    var outputStr = momentObj.format("YYYY-MM-DD HH:mm:ss");
 
-  return outputStr;
+    return outputStr;
 };
 
 export function convertDatetime(inputStr) {
-  if (!inputStr) return;
-  // Sử dụng Moment.js để phân tích chuỗi đầu vào
-  var momentObj = moment(inputStr, 'YYYY-MM-DD HH:mm:ss');
+    if (!inputStr) return;
+    // Sử dụng Moment.js để phân tích chuỗi đầu vào
+    var momentObj = moment(inputStr, "YYYY-MM-DD HH:mm:ss");
 
-  // Thêm 7 giờ vào thời gian
-  momentObj.add(7, 'hours');
+    // Thêm 7 giờ vào thời gian
+    momentObj.add(7, "hours");
 
-  return momentObj;
+    return momentObj;
 }
 
-export function convertDateAndFormat(inputStr, format = 'DD/MM/YYYY HH:mm:ss') {
-  if (!inputStr) return;
-  // Sử dụng Moment.js để phân tích chuỗi đầu vào
-  var momentObj = moment(inputStr, 'YYYY-MM-DD HH:mm:ss');
+export function convertDateAndFormat(inputStr, format = "DD/MM/YYYY HH:mm:ss") {
+    if (!inputStr) return;
+    // Sử dụng Moment.js để phân tích chuỗi đầu vào
+    var momentObj = moment(inputStr, "YYYY-MM-DD HH:mm:ss");
 
-  // Thêm 7 giờ vào thời gian
-  momentObj.add(7, 'hours');
+    // Thêm 7 giờ vào thời gian
+    momentObj.add(7, "hours");
 
-  return momentObj.format(format);
+    return momentObj.format(format);
 }
 
-export function convertDatetimeAndFormat(inputStr, format = 'DD/MM/YYYY HH:mm:ss') {
-  if (!inputStr) return;
-  // Sử dụng Moment.js để phân tích chuỗi đầu vào
-  var momentObj = moment(inputStr, 'YYYY-MM-DD HH:mm:ss');
+export function convertDatetimeAndFormat(
+    inputStr,
+    format = "DD/MM/YYYY HH:mm:ss"
+) {
+    if (!inputStr) return;
+    // Sử dụng Moment.js để phân tích chuỗi đầu vào
+    var momentObj = moment(inputStr, "YYYY-MM-DD HH:mm:ss");
 
-  // Thêm 7 giờ vào thời gian
-  momentObj.add(7, 'hours');
+    // Thêm 7 giờ vào thời gian
+    momentObj.add(7, "hours");
 
-  return momentObj.format(format);
+    return momentObj.format(format);
 }
